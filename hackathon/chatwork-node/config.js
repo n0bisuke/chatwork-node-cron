999dddaa74feea5df2e55dfcdb7c85501c62c6b7
sugawara ryousuke <rsksound@gmail.com>
2014-10-10 16:31:17 +0900

    first commit

--- hackathon/chatwork-node/config.js
+++ hackathon/chatwork-node/config.js
@@ -1,15 +1,10 @@
-//投稿メッセージ
-var message = '＝＝＝【集中タイムがはじまります】＝＝＝';
-var room_id = '*********';
-//投稿する時間
-var h = 11;
-h = h - 9;
-var cronTime = "0 30 "+h+" * * 1-5"; //平日11:30に投稿
-//var cronTime = "* * * * * *"; //毎秒
+var message = 'SampleText';
+var room_id = '----------------------';
+var cronTime = "* * * * * *";
 
 module.exports = {
 	'cw':{
-		'API_KEY': '**************************',
+		'API_KEY': '-------------------------',
 		'room_id': room_id,
 		'message': message
 	},
